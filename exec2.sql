e714a921038adc8dfb8164422539423038486b38
DongLingxiao <553979221@qq.com>
2019-03-08 17:28:42 +0800

    纯真ip地址直接转入数据库 666

--- exec2.sql
+++ exec2.sql
@@ -1,102 +1,30 @@
-# 1. 查找部门 30 中员工的详细信息
-select *from scott.emp
-where DEPTNO=30;
-# 2. 找出从事 clerk 工作的员工的编号、姓名、部门号
-show tables from scott;
-select *from scott.emp;
-select EMPNO,ENAME,DEPTNO from scott.emp
-where JOB='CLERK';
-# 3. 检索出奖金多于基本工资的员工信息
-select ENAME from emp
-where COMM>SAL;
-# 4. 检索出奖金多于基本工资 30% 员工信息
-select ENAME from emp
-where COMM>SAL*0.3;
-# 5. 希望看到 10 部门的经理或者 20 部门的职员 clerk 的信息
-select *
-from emp where (DEPTNO=10 and JOB='MANAGER')or(DEPTNO=20 and JOB='CLERK');
-
-# 6. 找出 10 部门的经理、20 部门的职员或者既不是经理也不是职员但是高于 2000 元的员工信息
-select *
-from emp where (DEPTNO=10 and JOB='MANAGER')OR (DEPTNO=20 AND JOB='CLERK')OR (JOB NOT IN ('MANAGER','CLERK')AND SAL>2000);
--- 一下是查找工资从2000到3000的 between and 的写法
-#select *
-#from emp where SAL between 2000 and 3000;
-# 7. 找出获得奖金的员工的工作
-select DISTINCT JOB
-from emp where COMM is not null;
-#select*
-#from emp where (COMM is not null)and (COMM>0);
-# 8. 找出奖金少于 100 或者没有获得奖金的员工的信息
-select *
-from emp where (COMM<100)or (COMM is null);
-
-# 9. 查找员工雇佣日期是当月的最后一天的员工信息
-
-# 10. 检索出雇佣年限超过 35 年的员工信息
-select HIREDATE
-from emp;
-show FULL columns from emp;
-# 11. 找出姓名以 A、B、S 开始的员工信息
-select *
-from emp
-where ENAME like 'A%'or ENAME like 'B%'or ENAME like 's%';
-# 12. 找到名字长度为 4 个字符的员工信息
-select *
-from emp
-where ENAME like '____';
-# 13. 名字中不包含 R 字符的员工信息
-select *
-from emp
-where ENAME not like '%R%';
-# 14. 找出员工名字的前3个字符
-select ENAME
-from emp;
-# 15. 将名字中 A 改为 a
-update emp
-set ENAME='a'where ENAME='A';
-select *
-from emp;
-# 16. 将员工的雇佣日期拖后 10 年
-
-# 17. 返回员工的详细信息并按姓名排序
-select *
-from emp order by ENAME;
-
-
-
-# 18. 返回员工的信息并按员工的工作年限降序排列
-select *
-from emp order by HIREDATE desc ;
-# 19. 返回员工的信息并按工作降序、工资升序排列
-select *
-from emp order by JOB desc,SAL;
-# 20. 返回员工的姓名、雇佣年份和月份，并按月份和雇佣日期排序
-select ENAME,HIREDATE
-from emp order by HIREDATE;
-# 21. 计算员工的日薪，每月按 30 天
-
-# 22. 找出 2 月份雇佣的员工
-# 23. 至今为止，员工被雇佣的天数
-# 24. 找出姓名中包含 A 的员工信息
-select *
-from emp
-where ENAME like 'A%';
-# 25. 计算出员工被雇佣了多少年、多少月、多少日
-#26.查出并计算员工基本工资加奖金 一共收入
-select ENAME,SAL+ifnull(COMM,0)
-from emp;
-
-#显示由键链接的员工和职位的两个表
+# 1. 返回拥有员工的部门名、部门号
 select e.ENAME, e.DEPTNO, d.DNAME, d.DEPTNO
 from emp e
-       inner join dept d on e.DEPTNO = d.DEPTNO;
--- 以上落选的表中信息显示   将inner改为right 或left （对于关键字右边或左边的文件落选的信息显示）
-select e.ENAME, e.DEPTNO, d.DNAME, d.DEPTNO
-from emp e
-       right join dept d on e.DEPTNO = d.DEPTNO
-union
-select e.ENAME, e.DEPTNO, d.DNAME, d.DEPTNO
-from emp e
-       left join dept d on e.DEPTNO = d.DEPTNO;
+            inner join dept d on e.DEPTNO = d.DEPTNO;
 
+# 2. 工资多于 scott 的员工信息
+select *
+from emp
+where SAL>(select SAL from emp where ENAME='scott');
+# 3. 返回员工和所属经理的姓名?
+select e.ENAME,d.ENAME
+from emp
+inner join on emp.DEPTNO = desc .DEPTNO;
+# 4. 返回雇员的雇佣日期早于其经理雇佣日期的员工及其经理姓名
+# 5. 返回员工姓名及其所在的部门名称
+# 6. 返回从事 clerk 工作的员工姓名和所在部门名称
+# 7. 返回部门号及其本部门的最低工资
+# 8. 返回销售部 sales 所有员工的姓名
+# 9. 返回工资多于平均工资的员工
+# 10. 返回与 scott 从事相同工作的员工
+# 11. 返回比 30 部门员工平均工资高的员工姓名与工资
+# 12. 返回工资高于30部门所有员工工资的员工信息
+# 13. 返回部门号、部门名、部门所在位置及其每个部门的员工总数
+# 14. 返回员工的姓名、所在部门名及其工资
+# 15. 返回雇员表中不在同一部门但是从事相同工作的员工信息
+# 16. 返回员工的详细信息，包括部门名
+# 17. 返回员工工作及其从事此工作的最低工资
+# 18. 返回不同部门经理的最低工资
+# 19. 计算出员工的年薪，并且以年薪排序
+# 20. 返回工资处于第 4 级别的员工的姓名
---
+++ id.sql
@@ -0,0 +1,22 @@
+drop database if exists db_ip;
+create database db_ip;
+
+drop table if exists db_ip.ip;
+create table db_ip.ip(
+
+  id int auto_increment primary key comment 'id主键',
+  min varchar(255)not null comment '最小ip',
+  max varchar(255)not null comment '最大ip',
+  loc varchar(255)not null comment '地址'
+)comment 'ip地址表';
+
+load data local infile 'D:/ip.txt'
+into table db_ip.ip
+fields terminated by '|'
+  (min,max,loc)
+set id=null;
+select *
+from db_ip.ip;
+
+update db_ip.ip
+set min=trim(min),max=trim(max),loc=trim(loc);
